e0b3b7f11efe0bbbf7a8c100a4a8b26d1c4937cb
ajn404 <nhy1423238345@163.com>
2025-02-10 15:38:36 +0800

    Update project dependencies and configuration for Vite, Tailwind CSS, and React Router

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
+import autoprefixer from 'autoprefixer';
 import path from "path"
+
 export default defineConfig({
   plugins: [react(), cesium()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(),
+        require('tailwindcss'),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
