9fed6645ec9a0ce3e332b6f4d7196e5875f1cef6
ajn404 <ajn404@users.noreply.github.com>
2025-02-17 10:30:00 +0000

    deploy: 201c7875a858ce8f75affa8c746ef508113554fa

---
+++ assets/tgaTextureLoader-Ch3pyCmK.js
@@ -0,0 +1,1 @@
+import{g as a,h as n}from"./PBR-DcZsS4zJ.js";import"./index-Cv0NdHz6.js";import"./useCesium-CdGbx-yj.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,s){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),r=a(o);s(r.width,r.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{f as _TGATextureLoader};
--- assets/tgaTextureLoader-xsm8Kswd.js
+++
@@ -1,1 +0,0 @@
-import{g as a,h as n}from"./PBR-BlsDW6OL.js";import"./index-BLrdrK_U.js";import"./useCesium-D_1Ofcts.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,s){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),r=a(o);s(r.width,r.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{f as _TGATextureLoader};
