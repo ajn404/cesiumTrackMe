7f519102914bee89347525cc1f62bc1f3b1a4251
ajn404 <ajn404@users.noreply.github.com>
2025-02-17 09:30:24 +0000

    deploy: de1a188a1c1c898d588c2d636d1d42c1ddbf09d3

---
+++ assets/tgaTextureLoader-CqXGJ6Pb.js
@@ -0,0 +1,1 @@
+import{g as a,h as n}from"./PBR-BCqSUh35.js";import"./index-DeNd8wiF.js";import"./useCesium-C1OWLYxi.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,s){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),r=a(o);s(r.width,r.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{f as _TGATextureLoader};
