645b892151248d2a39f30afb604ffceabca450af
ajn404 <ajn404@users.noreply.github.com>
2025-06-18 06:35:07 +0000

    deploy: 6c163a73631acb45e795956d97846f371e4923b7

---
+++ assets/CustomShader-wGexSO6t.js
@@ -0,0 +1,10 @@
+import{r as t,M as w,j as m}from"./index-CDxylz0H.js";import{u as C}from"./useCesium-Dqml_Yis.js";function x(){const{mapProvider:c}=t.useContext(w),{cesiumContainerRef:o,viewer:e}=C(void 0,{tianDiTu:{enabled:c==="Tianditu",token:"fa4fa7129232846bfb71e9c37848b6df"}}),i=t.useRef(null),l=t.useRef(null);return t.useEffect(()=>{if(e.current){i.current=e.current.scene;const n=new Cesium.Primitive({geometryInstances:new Cesium.GeometryInstance({geometry:new Cesium.RectangleGeometry({rectangle:Cesium.Rectangle.fromDegrees(-180,-90,180,90),vertexFormat:Cesium.VertexFormat.POSITION_AND_ST})}),appearance:new Cesium.MaterialAppearance({material:new Cesium.Material({fabric:{type:"LiquidGlass",uniforms:{u_mouse:new Cesium.Cartesian2(0,0),u_time:0,u_resolution:new Cesium.Cartesian2(window.innerWidth,window.innerHeight)},source:`
+uniform float u_time;
+czm_material czm_getMaterial(czm_materialInput materialInput)
+{
+    czm_material material = czm_getDefaultMaterial(materialInput);
+    material.diffuse = vec3(1.0, abs(sin(u_time*2.0)), abs(cos(u_time)));
+    material.alpha = 0.5 + 0.5 * sin(u_time * 0.5);
+    return material;
+}
+                        `}}),translucent:!0})});l.current=n,i.current.primitives.add(n);const a=e.current.canvas;new Cesium.ScreenSpaceEventHandler(a).setInputAction(r=>{const s=r.endPosition;if(s){const f=s.x/a.clientWidth,p=1-s.y/a.clientHeight;n.appearance.material.uniforms.u_mouse=new Cesium.Cartesian2(f,p)}},Cesium.ScreenSpaceEventType.MOUSE_MOVE);const d=Cesium.JulianDate.now().secondsOfDay;i.current.postRender.addEventListener(()=>{const r=Cesium.JulianDate.now().secondsOfDay;n.appearance.material.uniforms.u_time=r-d,e.current.scene.requestRender()});const u=()=>{n.appearance.material.uniforms.u_resolution=new Cesium.Cartesian2(window.innerWidth,window.innerHeight)};return window.addEventListener("resize",u),()=>{window.removeEventListener("resize",u)}}},[e]),m.jsx("div",{className:"h-full w-full",children:m.jsx("div",{ref:o,className:"h-full w-full"})})}export{x as default};
